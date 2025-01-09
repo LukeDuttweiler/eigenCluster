f74d3a6f8de28ea1d8c7979ea784619ab871b3e8
LukeDuttweiler <lduttweiler@hsph.harvard.edu>
2025-01-09 16:17:39 -0500

    Added the least squares consensus clustering

---
+++ R/leastSquares.R
@@ -0,0 +1,16 @@
+#Least Squares clustering
+leastSquares <- function(SList){
+  #Make co-association matrix
+  S <- Reduce('+', SList)/length(SList)
+
+  #Get least squares measure per clustering
+  lSqs <- sapply(SList, function(Si){
+    return(sum((Si - S)^2))
+  })
+
+  #Identify final clustering
+  finS <- SList[[which(lSqs == min(lSqs))[1]]]
+
+  g <- igraph::graph_from_adjacency_matrix(finS)
+  return(igraph::components(g)$membership)
+}
